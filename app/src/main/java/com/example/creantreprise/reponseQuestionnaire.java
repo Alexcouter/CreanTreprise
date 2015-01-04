e3e509589ecdd0a7f1d79f20db7b75d7f1383664
Alexcouter <alexcouter@gmail.com>
2015-01-04 18:06:35 +0100

    problemes

--- app/src/main/java/com/example/creantreprise/reponseQuestionnaire.java
+++ app/src/main/java/com/example/creantreprise/reponseQuestionnaire.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class reponseQuestionnaire extends Activity {
@@ -12,6 +14,14 @@ public class reponseQuestionnaire extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reponse_questionnaire);
+
+        final Button ok = (Button) findViewById(R.id.okreponse);
+        ok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 
 
