3e31156b3c782f01e762325007e89e7511812495
Alexcouter <alexcouter@gmail.com>
2015-01-06 11:56:12 +0100

    Debut sauvegarde questionnaire

--- app/src/main/java/com/example/creantreprise/Sarl.java
+++ app/src/main/java/com/example/creantreprise/Sarl.java
@@ -1,16 +1,16 @@
 package com.example.creantreprise;
 
-import com.example.creantreprise.util.SystemUiHider;
-
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+
+import com.example.creantreprise.util.SystemUiHider;
 
 
 /**
