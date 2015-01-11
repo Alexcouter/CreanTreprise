e6f9f89ed06eab8c96b84fc2cab2e503e5738b12
zhyroo <julien.gouablin@gmail.com>
2015-01-11 22:06:58 +0100

    Classes

--- app/src/main/java/com/example/creantreprise/reponseQuestionnaire2.java
+++ app/src/main/java/com/example/creantreprise/reponseQuestionnaire2.java
@@ -1,9 +1,11 @@
 package com.example.creantreprise;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
 
@@ -14,6 +16,12 @@ public class reponseQuestionnaire2 extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reponse_questionnaire2);
+
+        final Intent i1 = new Intent(reponseQuestionnaire2.this, reponse1.class);
+        final Intent i2 = new Intent(reponseQuestionnaire2.this, reponse2.class);
+        final Intent i3 = new Intent(reponseQuestionnaire2.this, reponse3.class);
+        final Intent i4 = new Intent(reponseQuestionnaire2.this, reponse4.class);
+        final Intent i5 = new Intent(reponseQuestionnaire2.this, reponse5.class);
 
         final RadioButton q1r1 = (RadioButton) findViewById(R.id.bq1r1);
         final RadioButton q1r2 = (RadioButton) findViewById(R.id.bq1r2);
@@ -28,7 +36,29 @@ public class reponseQuestionnaire2 extends Activity {
         final RadioButton q4r2 = (RadioButton) findViewById(R.id.bq4r2);
 
         final Button boutonSuivant = (Button) findViewById(R.id.Questionnaire2);
+
+        boutonSuivant.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (q2r2.isChecked()) {
+                    reponseQuestionnaire2.this.startActivity(i1);
+                } else if (q1r1.isChecked() && q3r2.isNotChecked) {
+                    reponseQuestionnaire2.this.startActivity(i2);
+                } else if (q1r1.isChecked() && q3r2.isChecked()) {
+                    reponseQuestionnaire2.this.startActivity(i3);
+                } else if (q1r2.isChecked() && q3r2.isNotChecked) {
+                    reponseQuestionnaire2.this.startActivity(i4);
+                } else if (q1r2.isChecked() && q3r2.isChecked()) {
+                    reponseQuestionnaire2.this.startActivity(i5);
+                }
+
+
+            }
+        }
     }
+
+
 
 
     @Override
